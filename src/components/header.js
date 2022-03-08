19e484fd96b9c48af2b24bac57e82da0e43f9f1f
Scott Lucas <8662611+scottmlucas@users.noreply.github.com>
2022-03-08 17:17:01 -0500

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+export const Header = () => (
+  <div>
+    I'm a header
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import { Header } from '../components/header';
+
+const Homepage = () => (
+  <div>
+    <Header />
+    <div className='flex justify-center'>
+      Content
+    </div>
+  </div>
+)
+
+export default Homepage;
